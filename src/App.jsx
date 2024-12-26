8ab3dd8a987f2639e637b37fbada89be3e873590
Anarbek <anarbekishembekuulu@gmail.com>
2024-12-26 14:43:18 +0600

    the beginning of the part of the model display

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Hero } from "./components/Hero/Hero";
 import { Highlights } from "./components/Highlights/Highlights";
+import { Model } from "./components/Model/Model";
 
 export const App = () => {
     return (
@@ -9,6 +10,7 @@ export const App = () => {
             <NavBar />
             <Hero />
             <Highlights />
+            <Model />
         </main>
     );
 };
