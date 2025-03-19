d81d8d5866d489819923b2dcbb294cb6fe4601de
Anarbek-front <anarbekishembekuulu@gmail.com>
2025-03-19 11:55:03 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
 
-    base: process.env.VITE_BASE_PATH || '/Apple_website_copy',
+    base: process.env.VITE_BASE_PATH || '/',
 
     server: {
         open: true,
