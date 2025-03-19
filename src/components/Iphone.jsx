05304cf900c88fe500eeb0a6c0c4dee027abc9e4
Anarbek-front <anarbekishembekuulu@gmail.com>
2025-03-19 11:40:21 +0600

    path_model

--- src/components/Iphone.jsx
+++ src/components/Iphone.jsx
@@ -11,7 +11,7 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
 
 function Model(props) {
-    const { nodes, materials } = useGLTF('/Apple_website_copy/models/scene.glb')
+    const { nodes, materials } = useGLTF('/models/scene.glb')
 
     const texture = useTexture(props.item.img)
 
@@ -257,4 +257,4 @@ function Model(props) {
 
 export default Model
 
-useGLTF.preload('/Apple_website_copy/models/scene.glb')
+useGLTF.preload('/models/scene.glb')
